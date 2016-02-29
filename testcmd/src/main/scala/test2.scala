555bb0a4a61410f0d2f1631229f4ad136eae3f85
Guillaume <guillaume.tache@hotmail.com>
2016-02-29 15:34:18 +0100

    Adds default task

---
+++ testcmd/src/main/scala/test2.scala
@@ -0,0 +1,17 @@
+import scala.scalajs.js
+import js.annotation.JSExport
+
+@JSExport
+object Test2 extends js.JSApp {
+  @JSExport
+  def main(): Unit = {
+    val squared = square(3)
+    println("Hello ! Square of 10 is "+squared)
+  }
+
+  /** Computes the square of an integer.
+    *  This demonstrates unit testing.
+    */
+  @JSExport
+  def square(x: Int): Int = x*x
+}
