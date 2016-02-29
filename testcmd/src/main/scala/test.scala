0efce67bf047e6f1b7961ca42ede64840b2d47fb
Guillaume <guillaume.tache@hotmail.com>
2016-02-29 11:59:18 +0100

    Adds argument for compileJS / compileSJSIR

---
+++ testcmd/src/main/scala/test.scala
@@ -0,0 +1,17 @@
+import scala.scalajs.js
+import js.annotation.JSExport
+
+@JSExport
+object Test extends js.JSApp {
+  @JSExport
+  def main(): Unit = {
+    val squared = square(10)
+    println("Hello ! Square of 10 is "+squared)
+  }
+
+  /** Computes the square of an integer.
+    *  This demonstrates unit testing.
+    */
+  @JSExport
+  def square(x: Int): Int = x*x
+}
