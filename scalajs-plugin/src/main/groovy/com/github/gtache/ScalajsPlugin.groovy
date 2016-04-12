fe94f5163240ed6664c58b35a904cec45f2dd820
Guillaume Tâche <Guillaume Tche>
2016-04-12 19:36:25 +0200

    Removes useless commented lines

--- scalajs-plugin/src/main/groovy/com/github/gtache/ScalajsPlugin.groovy
+++ scalajs-plugin/src/main/groovy/com/github/gtache/ScalajsPlugin.groovy
@@ -48,28 +48,19 @@ class ScalajsPlugin implements Plugin<Project> {
         cleanAll.toDelete = project.files(jsDir)
         project.logger.info('CleanAll task added')
 
-        /*
-        final def createDirs = tasks.create('CreateDirs', CreateDirsTask.class)
-        createDirs.toCreate = project.files(jsDir)
-        project.logger.info('CreateDirs task added')
-
-*/
         final def noOptJS = tasks.create('NoOptJS', CompileJSTask.class)
-        //noOptJS.dependsOn('CreateDirs')
         noOptJS.dependsOn('classes')
         noOptJS.destFile = jsFile
         noOptJS.noOpt()
         project.logger.info('NoOptJS task added')
 
         final def fastOptJS = tasks.create('FastOptJS', CompileJSTask.class)
-        //fastOptJS.dependsOn('CreateDirs')
         fastOptJS.dependsOn('classes')
         fastOptJS.destFile = jsFastFile
         fastOptJS.fastOpt()
         project.logger.info('FastOptJS task added')
 
         final def fullOptJS = tasks.create('FullOptJS', CompileJSTask.class)
-        //fullOptJS.dependsOn('CreateDirs')
         fullOptJS.dependsOn('classes')
         fullOptJS.destFile = jsFullFile
         fullOptJS.fullOpt()
