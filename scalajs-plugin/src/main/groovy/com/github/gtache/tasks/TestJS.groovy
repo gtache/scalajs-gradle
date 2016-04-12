763066b24bac7b75258b587bf0f52848d819abca
Guillaume Tâche <Guillaume Tche>
2016-04-12 20:53:51 +0200

    Continues stub for testing

--- scalajs-plugin/src/main/groovy/com/github/gtache/tasks/TestJS.groovy
+++ scalajs-plugin/src/main/groovy/com/github/gtache/tasks/TestJS.groovy
@@ -6,6 +6,10 @@ import org.gradle.api.tasks.TaskAction
 import org.scalajs.core.tools.logging.Level
 import org.scalajs.jsenv.ConsoleJSConsole$
 import org.scalajs.testadapter.ScalaJSFramework
+import org.scalajs.testadapter.ScalaJSRunner
+import sbt.testing.Logger
+import sbt.testing.Task
+import sbt.testing.TaskDef
 
 class TestJSTask extends DefaultTask {
     final String description = "Runs tests"
@@ -17,6 +21,12 @@ class TestJSTask extends DefaultTask {
                 Utils.resolveEnv(project),
                 Utils.resolveLogLevel(project, 'testLogLevel', Level.Debug$.MODULE$),
                 ConsoleJSConsole$.MODULE$)
+        final ScalaJSRunner runner = framework.runner(new String[0], new String[0], null)
+        final Task[] tasks = runner.tasks(new TaskDef[0])
+        for(Task t : tasks){
+            t.execute(null,new Logger[0])
+        }
+        framework.runDone()
     }
 
 
