72a7860b285599245a7dd2dfea3576f47863217d
Guillaume Tâche <Guillaume Tche>
2016-04-27 22:59:32 +0200

    Trying to fix testing

--- scalajs-plugin-test/src/test/scala/DummyObjectTest.scala
+++ scalajs-plugin-test/src/test/scala/DummyObjectTest.scala
@@ -1,12 +1,12 @@
 import org.junit.Test
 import org.junit.Assert._
 
-import scala.scalajs.js.annotation.JSExport
 
 class DummyObjectTest {
 
   @Test
-  def dummyTest() : Unit = {
-    assertEquals(100,DummyObject.square(10))
+  def dummyTest(): Unit = {
+    assertEquals(100, DummyObject.square(10))
+    assertNotEquals(100, DummyObject.square(5))
   }
 }
