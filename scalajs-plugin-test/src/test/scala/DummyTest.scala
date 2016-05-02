0b2a673e4ed6e3dd732aface164fe774ab3859a9
Guillaume Tâche <guillaume.tache@hotmail.com>
2016-05-02 15:47:41 +0200

    Adds scalatest to scalajs-plugin-test, still error

--- scalajs-plugin-test/src/test/scala/DummyTest.scala
+++
@@ -1,8 +0,0 @@
-import scala.scalajs.js.JSApp
-import scala.scalajs.js.annotation.JSExport
-
-@JSExport
-object DummyTest extends JSApp {
-  @JSExport
-  override def main(): Unit = println("test is on classpath")
-}
---
+++ scalajs-plugin-test/src/test/scala/ScalaTest.scala
@@ -0,0 +1,21 @@
+import org.scalatest._
+
+import scala.collection.mutable
+
+class ScalaTest extends FlatSpec {
+
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val stack = new mutable.Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    assert(stack.pop() === 2)
+    assert(stack.pop() === 1)
+  }
+
+  it should "throw NoSuchElementException if an empty stack is popped" in {
+    val emptyStack = new mutable.Stack[String]
+    intercept[NoSuchElementException] {
+      emptyStack.pop()
+    }
+  }
+}
