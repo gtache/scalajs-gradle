6be5b07cec828d7380e639337420f4735491608b
Guillaume Tâche <Guillaume Tche>
2016-05-17 22:29:34 +0200

    Adds several tests for ClassScanner

--- scalajs-plugin/src/test/scala/com/github/gtache/testing/ClassScannerTest.scala
+++ scalajs-plugin/src/test/scala/com/github/gtache/testing/ClassScannerTest.scala
@@ -8,70 +8,117 @@ import sbt.testing.{AnnotatedFingerprint, Fingerprint, SubclassFingerprint}
 
 class ClassScannerTest {
 
+  val packageName = "com.github.gtache.testing."
+
+  val annFingerprint = new AnnotatedFingerprint {
+    override def isModule: Boolean = true
+
+    override def annotationName(): String = packageName + "ToTest"
+  }
+
+  val subFingerprint = new SubclassFingerprint {
+    override def isModule: Boolean = true
+
+    override def superclassName(): String = packageName + "A"
+
+    override def requireNoArgConstructor(): Boolean = true
+  }
+
+  val subFingerprint2 = new SubclassFingerprint {
+    override def requireNoArgConstructor(): Boolean = false
+
+    override def isModule: Boolean = true
+
+    override def superclassName(): String = packageName + "G"
+  }
+
+  val subFingerprint3 = new SubclassFingerprint {
+    override def requireNoArgConstructor(): Boolean = true
+
+    override def isModule: Boolean = true
+
+    override def superclassName(): String = packageName + "K"
+  }
+
+  val fingerprints: Array[Fingerprint] = Array(annFingerprint, subFingerprint, subFingerprint2, subFingerprint3)
+  val test: URL = this.getClass.getResource("../../../../")
+  val loader = new URLClassLoader(Array(test))
+
+  val explicitelySpecified: Set[String] = Set(".*A.*", ".*B", ".*C")
+  val excluded = Set(".*C", ".*H")
+  val excludedAll: Set[String] = Set("com.*")
+  val all = Set(packageName + "A", packageName + "A$", packageName + "B", packageName + "C", packageName + "D",
+    packageName + "E", packageName + "F", packageName + "G", packageName + "H", packageName + "I", packageName + "J",
+    packageName + "K", packageName + "L")
+
+  def checkContains(nameTasks: Set[String], contained: Set[String], all: Set[String]): Unit = {
+    contained.foreach { s =>
+      assertTrue(nameTasks.contains(s))
+    }
+    all.filterNot(contained).foreach { s =>
+      assertFalse(nameTasks.contains(s))
+    }
+  }
+
   @Test
-  def testScanner(): Unit = {
-    val annFingerprint = new AnnotatedFingerprint {
-      override def isModule: Boolean = true
-
-      override def annotationName(): String = "com.github.gtache.testing.ToTest"
-    }
-
-    val subFingerprint = new SubclassFingerprint {
-      override def isModule: Boolean = true
-
-      override def superclassName(): String = "com.github.gtache.testing.A"
-
-      override def requireNoArgConstructor(): Boolean = true
-    }
-
-    val subFingerprint2 = new SubclassFingerprint {
-      override def requireNoArgConstructor(): Boolean = false
-
-      override def isModule: Boolean = true
-
-      override def superclassName(): String = "com.github.gtache.testing.G"
-    }
-
-    val subFingerprint3 = new SubclassFingerprint {
-      override def requireNoArgConstructor(): Boolean = true
-
-      override def isModule: Boolean = true
-
-      override def superclassName(): String = "com.github.gtache.testing.K"
-    }
-
-    val fingerprints: Array[Fingerprint] = Array(annFingerprint, subFingerprint, subFingerprint2, subFingerprint3)
-    val test: URL = this.getClass.getResource("../../../../")
-    val loader = new URLClassLoader(Array(test))
+  def testScannerBasic(): Unit = {
     val taskDefs = ClassScanner.scan(loader, fingerprints)
 
     val nameTasks = taskDefs.map(t => t.fullyQualifiedName())
-    assertTrue(nameTasks.contains("com.github.gtache.testing.A"))
-    assertTrue(nameTasks.contains("com.github.gtache.testing.A$"))
-    assertTrue(nameTasks.contains("com.github.gtache.testing.B"))
-    assertTrue(nameTasks.contains("com.github.gtache.testing.C"))
-    assertTrue(nameTasks.contains("com.github.gtache.testing.D"))
-    assertFalse(nameTasks.contains("com.github.gtache.testing.E"))
-    assertTrue(nameTasks.contains("com.github.gtache.testing.F"))
-    assertTrue(nameTasks.contains("com.github.gtache.testing.G"))
-    assertTrue(nameTasks.contains("com.github.gtache.testing.H"))
-    assertFalse(nameTasks.contains("com.github.gtache.testing.I"))
-    assertTrue(nameTasks.contains("com.github.gtache.testing.J"))
-    assertTrue(nameTasks.contains("com.github.gtache.testing.K"))
-    assertFalse(nameTasks.contains("com.github.gtache.testing.L"))
+    val contained = Set(packageName + "A", packageName + "A$", packageName + "B", packageName + "C", packageName + "D", packageName + "F",
+      packageName + "G", packageName + "H", packageName + "J", packageName + "K")
+    checkContains(nameTasks.toSet, contained, all)
+
 
     val map = nameTasks.zip(taskDefs).toMap
-    assertTrue(map.get("com.github.gtache.testing.A").get.fingerprint.isInstanceOf[SubclassFingerprint])
-    assertTrue(map.get("com.github.gtache.testing.A$").get.fingerprint.isInstanceOf[SubclassFingerprint])
-    assertTrue(map.get("com.github.gtache.testing.B").get.fingerprint.isInstanceOf[SubclassFingerprint])
-    assertTrue(map.get("com.github.gtache.testing.C").get.fingerprint.isInstanceOf[SubclassFingerprint])
-    assertTrue(map.get("com.github.gtache.testing.D").get.fingerprint.isInstanceOf[AnnotatedFingerprint])
-    assertTrue(map.get("com.github.gtache.testing.F").get.fingerprint.isInstanceOf[AnnotatedFingerprint])
-    assertTrue(map.get("com.github.gtache.testing.G").get.fingerprint.isInstanceOf[SubclassFingerprint])
-    assertTrue(map.get("com.github.gtache.testing.H").get.fingerprint.isInstanceOf[SubclassFingerprint])
-    assertTrue(map.get("com.github.gtache.testing.J").get.fingerprint.isInstanceOf[SubclassFingerprint])
-    assertTrue(map.get("com.github.gtache.testing.K").get.fingerprint.isInstanceOf[SubclassFingerprint])
+    assertTrue(map.get(packageName + "A").get.fingerprint.isInstanceOf[SubclassFingerprint])
+    assertTrue(map.get(packageName + "A$").get.fingerprint.isInstanceOf[SubclassFingerprint])
+    assertTrue(map.get(packageName + "B").get.fingerprint.isInstanceOf[SubclassFingerprint])
+    assertTrue(map.get(packageName + "C").get.fingerprint.isInstanceOf[SubclassFingerprint])
+    assertTrue(map.get(packageName + "D").get.fingerprint.isInstanceOf[AnnotatedFingerprint])
+    assertTrue(map.get(packageName + "F").get.fingerprint.isInstanceOf[AnnotatedFingerprint])
+    assertTrue(map.get(packageName + "G").get.fingerprint.isInstanceOf[SubclassFingerprint])
+    assertTrue(map.get(packageName + "H").get.fingerprint.isInstanceOf[SubclassFingerprint])
+    assertTrue(map.get(packageName + "J").get.fingerprint.isInstanceOf[SubclassFingerprint])
+    assertTrue(map.get(packageName + "K").get.fingerprint.isInstanceOf[SubclassFingerprint])
   }
+
+  @Test
+  def testScannerExplicitely(): Unit = {
+    val taskDefs = ClassScanner.scan(loader, fingerprints, explicitelySpecified)
+    val nameTasks = taskDefs.map(t => t.fullyQualifiedName())
+    val contained = Set(packageName + "A", packageName + "A$", packageName + "B", packageName + "C")
+    checkContains(nameTasks.toSet, contained, all)
+
+    val map = nameTasks.zip(taskDefs).toMap
+    assertTrue(map.get(packageName + "A").get.fingerprint.isInstanceOf[SubclassFingerprint])
+    assertTrue(map.get(packageName + "A$").get.fingerprint.isInstanceOf[SubclassFingerprint])
+    assertTrue(map.get(packageName + "B").get.fingerprint.isInstanceOf[SubclassFingerprint])
+    assertTrue(map.get(packageName + "C").get.fingerprint.isInstanceOf[SubclassFingerprint])
+  }
+
+  @Test
+  def testScannerExcluded(): Unit = {
+    val taskDefs = ClassScanner.scan(loader, fingerprints, explicitelySpecified, excluded)
+    val nameTasks = taskDefs.map(t => t.fullyQualifiedName())
+    val contained = Set(packageName + "A", packageName + "A$", packageName + "B")
+    checkContains(nameTasks.toSet, contained, all)
+
+    val map = nameTasks.zip(taskDefs).toMap
+    assertTrue(map.get(packageName + "A").get.fingerprint.isInstanceOf[SubclassFingerprint])
+    assertTrue(map.get(packageName + "A$").get.fingerprint.isInstanceOf[SubclassFingerprint])
+    assertTrue(map.get(packageName + "B").get.fingerprint.isInstanceOf[SubclassFingerprint])
+  }
+
+  @Test
+  def testScannerExcludedAll(): Unit = {
+    var taskDefs = ClassScanner.scan(loader, fingerprints, explicitelySpecified, excludedAll)
+    assertTrue(taskDefs.isEmpty)
+    taskDefs = ClassScanner.scan(loader, fingerprints, Set.empty, excludedAll)
+    assertTrue(taskDefs.isEmpty)
+  }
+
+
 }
 
 class A
